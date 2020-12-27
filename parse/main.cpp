f5d8be83727328e2aa0c65418cd1db343d20e4bc
Kartikey Tewari <kartikeya30@gmail.com>
2020-12-27 16:41:21 +0530

    Added the parse folder.
    This is the code that takes filtered json from insomnia
    and generated the number of distinct manufacturers in the
    database at that point of time. This helps in calculating
    the load time in case of a cache miss.

---
+++ parse/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define endl "\n"
+#define n 18881
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    map<string,bool> alpha;
+    for (int i=0;i<=n-1;i++)
+    {
+        string s;
+        cin >> s;
+        alpha[s]=true;
+    }
+
+    for (auto it=alpha.begin();it!=alpha.end();it++)
+    {
+        cout << it->first << endl;
+    }
+
+    return 0;
+}
